c2ccf2310e129c2ece5f6c3fe579bc1c02b4df62
Tapiwa Robson Zireva <tapiwazireva@gmail.com>
2025-12-02 21:24:33 +0200

    base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/To-Do/",
+  base: process.env.VERCEL ? "/" : "/To-Do/",
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
